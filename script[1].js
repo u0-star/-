a3dfc5c26c206b20354406aae177c1bede22d78d
u0-star <yu0909037839@gmail.com>
2026-09-07 14:46:21 +0800

    Delete script[1].js

--- script[1].js
+++
@@ -1,4 +0,0 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => { if (entry.isIntersecting) entry.target.classList.add("show"); });
-}, { threshold: 0.12 });
-document.querySelectorAll(".reveal").forEach(el => observer.observe(el));
